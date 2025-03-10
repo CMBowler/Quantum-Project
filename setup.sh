92fa444599e68a3a14b3177d52993f77529ceeb6
cmbowler <cbowler@tcd.ie>
2025-03-10 11:36:51 +0000

    Env setup: Adding gitignore, .vscode

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-source env/bin/activate
+ENV="env"
 
-pip install -r requirements.txt
+# Check if the directory exists
+if [ ! -d $ENV ]; then
+  # If it doesn't exist, create it
+  mkdir -p $ENV
+  python -m venv $ENV
+  pip install -r requirements.txt
+  echo "Environment created: $ENV"
+else
+  echo "Environment already exists: $ENV"
+fi
+
+source $ENV/bin/activate
